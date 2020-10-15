9beb4c481d659b8ac4e3363b0031bb4e985e9bef
bionic790 <53811382+bionic790@users.noreply.github.com>
2020-10-15 15:34:08 +0530

    Update GROSS SALARY.sh

--- GROSS SALARY.sh
+++ GROSS SALARY.sh
@@ -1,3 +1,4 @@
-read -p "Enter salary: " sal
-gros_sal=`echo "$sal*0.40+$sal*0.20" | bc -l`
-echo "Gross Salary is: $gros_sal"
+echo -n "Enter Basic salary: "
+read sal
+gros_sal=`echo "$sal+$sal*0.10+$sal*0.20" | bc -l`
+echo "Gross Salary is: "$gros_sal
